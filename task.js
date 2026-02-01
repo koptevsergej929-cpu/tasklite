a7147246adc353b9b99df1ea4ec94206dc6ce19a
Pasha <koptevpavel929@gmail.com>
2026-02-01 20:42:37 +0300

    Added homework

--- task.js
+++ task.js
@@ -15,3 +15,15 @@ if (tasks === 0){
 }else{
     console.log("Много задач");
 }
+
+let amount = 0;
+
+if (amount == 0){
+    console.log("Корзина пуста");
+}else if (amount <= 1000){
+    console.log("Скидки не применяются");
+}else if (amount >= 1000, amount <= 5000){
+    console.log("Скидка 5%");
+}else {
+    console.log("Скидка 10%");
+}
