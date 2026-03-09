c3d8b93f3af217d8a273671a5a29d24cf52f06f0
Pasha <koptevpavel929@gmail.com>
2026-03-09 15:24:10 +0300

    changer the task.js and style.css

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-const now = new Date();
-console.log(now);
+// const now = new Date();
+// console.log(now);
 
-const day = now.getDate();
-const month = now.getMonth() + 1;
-const year = now.getFullYear();
+// const day = now.getDate();
+// const month = now.getMonth() + 1;
+// const year = now.getFullYear();
 
 
-const hours = now.getHours();
-const minutes = now.getMinutes();
-const seconds = now.getSeconds();
+// const hours = now.getHours();
+// const minutes = now.getMinutes();
+// const seconds = now.getSeconds();
 
-console.log(`${day}.${month}.${year}.${hours}.${minutes}.${seconds}`);
-console.log(now.toLocaleString());
+// console.log(`${day}.${month}.${year}.${hours}.${minutes}.${seconds}`);
+// console.log(now.toLocaleString());
 
-function FormatDate(date) {
-    const d = date.getDate().toString().padStart[2, '0'];
-    const m = (date.getMonth() + 1).toString().padStart[2, '0'];
-    const y = date.getFullYear();
+// console.log("----------------------------------------------------------");
 
+const D = new Date().getHours();
+
+function watch(){
+    if (D >= 6 && D < 12){
+        return "Утро";
+    } else if (D >= 12 && D < 18){
+        return "День";
+    } else if (D >= 18 && D < 24){
+        return "Вечер";
+    } else {
+        return "Ночь";
+    }
 }
+console.log(watch());
